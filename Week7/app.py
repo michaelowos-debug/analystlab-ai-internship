0699623ffe69cc6771141088b1370e95f74164a4
Michael Favour Owonubi <michaelowos@gmail.com>
2026-08-31 20:49:34 +0100

    Add files via upload

---
+++ Week7/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open('model.pkl', 'rb'))
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+
+st.title("Review Sentiment Predictor")
+
+review = st.text_area("Enter a product review:")
+
+if st.button("Predict Sentiment"):
+    vec = tfidf.transform([review])
+    prediction = model.predict(vec)[0]
+    st.subheader(f"Predicted Sentiment: {prediction}")
